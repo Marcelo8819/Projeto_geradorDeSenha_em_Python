c54d0a39cbcd19b8456f1cae4f35e1ef8c84999b
Marcelo8819 <silva.marcelohg@gmail.com>
2022-09-27 10:38:48 -0300

    first commit

---
+++ password_generator.py
@@ -0,0 +1,14 @@
+import random  # serve para importar um número aleatório 
+import string  # é a biblioteca com opções de senhas disponiveis 
+
+def password_generator(Len_pass = 8):   # def serve para definir o nome da funçaõ em python 
+    ascii_options = string.ascii_letters
+    number_options = string.digits
+    punt_options = string.punctuation
+    options = ascii_options + number_options + punt_options 
+
+    
+
+
+
+
